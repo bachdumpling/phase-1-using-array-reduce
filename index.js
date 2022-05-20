418bd24186e0528ddd82b6b58d73883a12a351a6
Bach Le <lehoangbach7802@gmail.com>
2022-05-20 11:19:07 -0400

    commit tree 5eba2ce5acb08f1b9b77d02eeb37f4cd987a1784

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue;
+}, 0);
+
+// function totalBatteries(array, combine, start) {
+//     let sum = start
+//     for (let element of array) {
+//         sum = combine(sum, element)
+//     }
+//     return sum;
+// }
+
+// console.log(totalBatteries(batteryBatches, (a, b) => a + b, 0));
+
